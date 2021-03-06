9b789768fdfb2dcc8fac96b67845c0fd0a576ce5
Clayon Stewart <c.stewart89@gmail.com>
2021-03-06 19:23:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   if block_given?
     i = 0
     list = []
-    while i< collection.length
+    while i < collection.length
       list << yield collection[i]
     end 
     list
